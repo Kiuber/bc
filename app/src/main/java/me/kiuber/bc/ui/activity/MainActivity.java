e8b781afab77fb2c1db80f5949d7001555e63cf2
Kiuber <Kiuber.Zhang@gmail.com>
2017-02-27 11:46:56 +0800

    +TAB

--- app/src/main/java/me/kiuber/bc/ui/activity/MainActivity.java
+++ app/src/main/java/me/kiuber/bc/ui/activity/MainActivity.java
@@ -1,15 +1,88 @@
 package me.kiuber.bc.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
+
+import java.util.ArrayList;
 
 import me.kiuber.bc.R;
+import me.kiuber.bc.ui.fragment.DynamicFragment;
+import me.kiuber.bc.ui.fragment.FunctionFragment;
+import me.kiuber.bc.ui.fragment.MyFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewPager mVpContent;
+    private RadioButton mRbFunction;
+    private RadioButton mRbDynamic;
+    private RadioButton mRbMy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mVpContent = (ViewPager) findViewById(R.id.vp_content);
+        mRbFunction = (RadioButton) findViewById(R.id.rb_function);
+        mRbDynamic = (RadioButton) findViewById(R.id.rb_dynamic);
+        mRbMy = (RadioButton) findViewById(R.id.rb_my);
+
+        mRbFunction.setOnClickListener(this);
+        mRbDynamic.setOnClickListener(this);
+        mRbMy.setOnClickListener(this);
+
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(new FunctionFragment());
+        fragments.add(new DynamicFragment());
+        fragments.add(new MyFragment());
+        FragmentPagerAdapter fragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager(), fragments);
+        mVpContent.setAdapter(fragmentPagerAdapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rb_function:
+                mVpContent.setCurrentItem(0);
+                break;
+            case R.id.rb_dynamic:
+                mVpContent.setCurrentItem(1);
+                break;
+            case R.id.rb_my:
+                mVpContent.setCurrentItem(2);
+                break;
+        }
+    }
+
+    private class FragmentPagerAdapter extends android.support.v4.app.FragmentPagerAdapter {
+        ArrayList<Fragment> fragments;
+
+        private FragmentPagerAdapter(FragmentManager fm, ArrayList<Fragment> fragments) {
+            super(fm);
+            this.fragments = fragments;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+//        @Override
+//        public Object instantiateItem(ViewGroup container, int position) {
+//            return super.instantiateItem(container, position);
+//        }
     }
 }
---
+++ app/src/main/java/me/kiuber/bc/ui/fragment/FunctionFragment.java
@@ -0,0 +1,23 @@
+package me.kiuber.bc.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.kiuber.bc.R;
+
+/**
+ * Created by Kiuber on 2017/2/27 0027.
+ */
+
+public class FunctionFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_function, null);
+        return view;
+    }
+}
